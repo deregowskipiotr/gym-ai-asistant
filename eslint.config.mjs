c77c06491e5945959bc20335ef45ef28d1d49950
Piotr Deregowski <deregowskipiotr@interia.pl>
2025-05-07 20:29:57 +0200

    trying add usercard image

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,9 +14,9 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      
-    }
-  }
+      "@next/next/no-img-element": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
